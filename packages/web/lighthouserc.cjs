ba43665ee8efe4d0d5db79576d7fb12e7b93fd0c
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2026-08-27 10:38:28 -0400

    fix(ci): load Lighthouse configuration under ESM packages

--- packages/web/lighthouserc.cjs
+++ packages/web/lighthouserc.cjs
@@ -10,13 +10,11 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
-        // Relaxing some strict PWA requirements for initial launch
         'service-worker': 'off',
         'installable-manifest': 'off',
         'splash-screen': 'off',
         'themed-omnibox': 'off',
         'maskable-icon': 'off',
-        // Performance targets
         'first-contentful-paint': ['warn', { maxNumericValue: 1000 }],
         'largest-contentful-paint': ['warn', { maxNumericValue: 1500 }],
         'interactive': ['warn', { maxNumericValue: 2000 }],
